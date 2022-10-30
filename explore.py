5bb7322ce11968870dddaf98de85889736b9dbf7
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-10-30 13:32:47 -0500

    update code comments

--- explore.py
+++ explore.py
@@ -8,37 +8,54 @@ import seaborn as sns
 
 def get_churn_mean_bar(df):
     ''' This function takes in telco data frame and returns a histoplot that
-    graphs the percentage of Telco customer who have churn'''
+        graphs the percentage of Telco customer who have churn
+    '''
     
+    # set backgorund and text scale
     sns.set(font_scale=1.3)  
     sns.set_style('white')
     
+    # calculate population of churn
     c_percent = round(df.churn.value_counts(normalize=True)[1],3)* 100
+
+    #title
     plt.title(f'Customers churn by {c_percent}%',fontsize=25,fontweight=100,color='midnightblue')
+
+    #Create histoplot and set parameters
     sns.histplot(data =df, x='churn',stat='percent',hue='churn', palette='cubehelix')
     col_mean= round(df.churn.value_counts(normalize=True)[1],3)* 100
     plt.axhline(col_mean, label = 'Churn Rate',color='midnightblue',linestyle='dashed')
-   
     plt.show();
 
 ########################### MONTHLY CHARGES #############################################
 
 def get_monthly_charges(df):
+    ''' This function takes in telco data frame and returns a boxplot that
+        shows the difference in total charges means.
+    '''
    
+    # set backgorund and text scale
     sns.set(font_scale=1.3)  
     sns.set_style('white')
 
-    colors = ['#6BAF8E', '#E6AFC9']
     # Set your custom color palette
+    colors = ['#6BAF8E', '#E6AFC9']
     sns.set_palette(sns.color_palette(colors))
-    plt.title('Higher mean Monthly Charge',fontsize=25,fontweight=100,color='midnightblue')
+
+    # create title 
+    plt.title('Slightly Higher mean Monthly Charge',fontsize=25,fontweight=100,color='midnightblue')
+
+    # create boxplot and set parameters
     sns.boxplot(y=df.monthly_charges, x=df.churn,
                 whis=np.inf
                )
 
 
 def get_ttest_monthly_charges(df):
-    
+    ''' This function takes in telco data frame and returns  the results of
+        a one- tailed T-Test
+    '''
+
     # create two independent sample group of customers: churn and not churn.
     subset_churn =df[df.churn=='Yes']
     subset_notchurn = df[df.churn =='No']
@@ -62,13 +79,15 @@ def get_ttest_monthly_charges(df):
     # Round and print results
     t_stat = t_stat.round(4)
     p_val = (p_val.round(4))/2
-    print(f' t-stat:{t_stat}')
-    print(f' p-value/2:{p_val/2}')
+    print(f' t-stat:  {t_stat}')
+    print(f' p-value/2:  {p_val/2}')
 
 ############################## SENIOR CITIZENS ###################################
 
 def get_bar_senior(df):
-  
+    ''' This function takes in telco data frame and returns a two countplots that
+        shows senior citizens vs churned.
+    '''  
     plt.figure(figsize=(10,4))
 
     # change encoding of senior citizen to text
@@ -80,10 +99,11 @@ def get_bar_senior(df):
     sns.set_palette(sns.color_palette(colors))
     sns.set_style('white')
     
+    # plot count plot
     plt.subplot(1,2,1)
     sns.countplot(x=senior, data= df, hue = 'churn')
 
-
+    # plot countplot overlay
     plt.subplot(1,2,2)
     sns.countplot(x=senior, data= df, hue = 'churn',dodge=False)
 
@@ -91,9 +111,11 @@ def get_bar_senior(df):
     plt.suptitle('Senior Citizens Churn More',fontsize=25,fontweight=100,color='midnightblue')
     plt.show();
 
-def get_chi2_senior(df):    
-    # Chi-Square test to compare two categorical variables (senior citizen status, churn)
 
+def get_chi2_senior(df):    
+    '''This function takes in a dataframe; conducts a Chi-Square test to compare two categorical variables 
+        (senior citizen status, churn) and returns the results
+    '''
     # Set alpha to 0.05
     alpha = 0.05
 
@@ -111,7 +133,10 @@ def get_chi2_senior(df):
 
 ############################################## TENURE #########################################
 def get_boxplot_tenure(df):  
-
+    ''' This function takes in telco data frame and returns a boxplot that
+        shows the difference in tenure means.
+    '''
+    # Set font size and background color
     sns.set(font_scale=1.3)  
     sns.set_style('white')
 
@@ -119,8 +144,14 @@ def get_boxplot_tenure(df):
     colors = ['#6BAF8E', '#E6AFC9']
     sns.set_palette(sns.color_palette(colors))
 
-    plt.title('Lower mean tenure')
-    sns.boxplot(y=df.tenure, x=df.churn,whis=np.inf);
+    # title
+    plt.title('Lower mean Tenure for churned customers',fontsize=25,fontweight=100,color='midnightblue')
+
+    # plot boxplot
+    sns.boxplot(y=df.tenure, x=df.churn,whis=np.inf)
+    plt.show();
+
+
 
 def get_ttest_tenure(df):
     
@@ -155,17 +186,31 @@ def get_ttest_tenure(df):
 ################################ CONTRACT TYPE #########################################
 
 def get_plot_contract(df):
+    ''' This function takes in telco data frame and returns a countplot that
+        shows tenure vs churned.
+    '''   
+
+    # set font scale and backgound color   
     sns.set(font_scale=1.3)  
     sns.set_style('white')
+
     # Set your custom color palette
     colors = ['#6BAF8E', '#E6AFC9']
     sns.set_palette(sns.color_palette(colors))
 
-    plt.title('Contract Type vs Churn')
-    sns.countplot(x=df.contract_type, data=df, hue = 'churn');
+    # title
+    plt.title('Contract Type vs Churn',fontsize=25,fontweight=100,color='midnightblue')
 
-def get_chi2_contract(df):    
-    # Chi-Square test to compare two categorical variables (contract type, churn)
+    # plot countplot
+    sns.countplot(x=df.contract_type, data=df, hue = 'churn')
+    plt.show();
+
+
+
+def get_chi2_contract(df): 
+    '''This function takes in a dataframe; conducts a Chi-Square test to compare two categorical variables 
+        (contract type, churn) and returns the results
+    '''   
     # Set alpha to 0.05
     alpha = 0.05
 
@@ -184,6 +229,7 @@ def get_chi2_contract(df):
 #################################### GENDER ###################################
 def get_plot_gender(df):  
 
+    # set font scale and background color
     sns.set(font_scale=1.3)  
     sns.set_style('white')
 
@@ -191,13 +237,19 @@ def get_plot_gender(df):
     colors = ['#6BAF8E', '#E6AFC9']
     sns.set_palette(sns.color_palette(colors))
 
-    plt.title('Gender Vs Churn')
-    sns.countplot(x=df.gender, data=df, hue = 'churn')
+    # title
+    plt.title('Gender not a driver of churn',fontsize=25,fontweight=100,color='midnightblue')
 
+    # plot count plot
+    sns.countplot(x=df.gender, data=df, hue = 'churn')
     plt.legend()
     plt.show();
 
-def get_chi2_gender(df):    
+
+def get_chi2_gender(df): 
+    '''This function takes in a dataframe; conducts a Chi-Square test to compare two categorical variables 
+        (gender, churn) and returns the results
+    '''   
     # Chi-Square test to compare two categorical variables (gender, churn)
     # Set alpha to 0.05
     alpha = 0.05
@@ -218,6 +270,7 @@ def get_chi2_gender(df):
 
 def get_plot_partner(df):  
 
+    # set font scale and backgournd style
     sns.set(font_scale=1.3)  
     sns.set_style('white')
 
@@ -225,14 +278,18 @@ def get_plot_partner(df):
     colors = ['#6BAF8E', '#E6AFC9']
     sns.set_palette(sns.color_palette(colors))
 
-    plt.title('Partner Vs Churn')
+    # title
+    plt.title('Partner Vs Churn',fontsize=25,fontweight=100,color='midnightblue')
+   
+    # plot countplot
     sns.countplot(x=df.partner, data=df, hue = 'churn')
-
     plt.legend()
     plt.show();
 
 def get_chi2_partner(df):    
-    # Chi-Square test to compare two categorical variables (gender, churn)
+    '''This function takes in a dataframe; conducts a Chi-Square test to compare two categorical variables 
+        (partner, churn) and returns the results
+    '''
     # Set alpha to 0.05
     alpha = 0.05
 
